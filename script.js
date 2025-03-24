aaa34762d1b2a6c6eff0ad0fe5f75a4e6dd21f25
Zynkah <ffgurl07@yahoo.com>
2025-03-24 17:00:39 -0500

    canvas added

---
+++ script.js
@@ -0,0 +1,18 @@
+function draw() {
+  const canvas = document.getElementById("canvas");
+  if (canvas.getContext) {
+    const ctx = canvas.getContext("2d");
+
+    // for (let i = 0; i < 6; i++) {
+    //   for (let j = 0; j < 6; j++) {
+    //     ctx.strokeStyle = `rgb(0 ${Math.floor(255 - 42.5 * i)} ${Math.floor(
+    //       255 - 42.5 * j
+    //     )})`;
+    //     ctx.beginPath();
+    //     ctx.arc(12.5 + j * 25, 12.5 + i * 25, 10, 0, 2 * Math.PI, true);
+    //     ctx.stroke();
+    //   }
+    // }
+  }
+}
+window.addEventListener("load", draw);
