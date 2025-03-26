3a6392b1376a090d7c77c31f51ae97b7a1849a47
Zynkah <ffgurl07@yahoo.com>
2025-03-26 16:42:48 -0500

    castle started

--- script.js
+++ script.js
@@ -3,16 +3,108 @@ function draw() {
   if (canvas.getContext) {
     const ctx = canvas.getContext("2d");
 
-    // for (let i = 0; i < 6; i++) {
-    //   for (let j = 0; j < 6; j++) {
-    //     ctx.strokeStyle = `rgb(0 ${Math.floor(255 - 42.5 * i)} ${Math.floor(
-    //       255 - 42.5 * j
-    //     )})`;
-    //     ctx.beginPath();
-    //     ctx.arc(12.5 + j * 25, 12.5 + i * 25, 10, 0, 2 * Math.PI, true);
-    //     ctx.stroke();
-    //   }
-    // }
+    // x,y, width, hieght
+    // center tower
+    ctx.strokeRect(79, 100, 45, 70); //lower
+    ctx.strokeRect(75, 89, 52, 10); //lower cap
+
+    ctx.strokeRect(92, 28, 20, 60); //upper
+    ctx.beginPath();
+    ctx.moveTo(88, 14);
+    ctx.lineTo(93, 14);
+    ctx.lineTo(93, 18);
+    ctx.lineTo(97, 18);
+    ctx.lineTo(97, 14);
+    ctx.lineTo(99, 14);
+    ctx.lineTo(99, 14);
+    ctx.lineTo(104, 14);
+    ctx.lineTo(105, 14);
+    ctx.lineTo(105, 18);
+    ctx.lineTo(110, 18);
+    ctx.lineTo(110, 14);
+    ctx.lineTo(112, 14);
+    ctx.lineTo(112, 18);
+    ctx.lineTo(116, 18);
+    ctx.lineTo(116, 14);
+    ctx.lineTo(116, 27);
+    ctx.lineTo(88, 27);
+    ctx.closePath();
+    ctx.stroke();
+    // ctx.strokeRect(89, 13, 26, 14); //upper cap
+
+    // middle towers
+    ctx.strokeRect(37, 130, 40, 40); //left
+    ctx.strokeRect(37, 114, 40, 14); // left cap
+
+    ctx.strokeRect(126, 130, 40, 40); //right
+    ctx.strokeRect(126, 114, 40, 14); //right cap
+
+    // center left tower side
+    ctx.beginPath();
+    ctx.moveTo(69, 115);
+    ctx.lineTo(69, 60);
+    ctx.lineTo(93, 60);
+    // center right tower side
+    ctx.moveTo(135, 115);
+    ctx.lineTo(135, 60);
+    ctx.lineTo(113, 60);
+    ctx.stroke();
+
+    ctx.strokeRect(69, 45, 23, 14); // center left tower cap
+    ctx.strokeRect(113, 45, 23, 14); // center right tower cap
+
+    // center outer top towers
+    ctx.strokeRect(69, 29, 10, 15); // left
+    // left cap
+    ctx.beginPath();
+    ctx.moveTo(67, 22);
+    ctx.lineTo(70, 22);
+    ctx.lineTo(70, 25);
+    ctx.lineTo(74, 25);
+    ctx.lineTo(74, 22);
+    ctx.lineTo(78, 22);
+    ctx.lineTo(78, 25);
+    ctx.lineTo(80, 25);
+    ctx.lineTo(81, 22);
+    ctx.lineTo(81, 30);
+    ctx.lineTo(67, 30);
+    ctx.closePath();
+    ctx.stroke();
+
+    ctx.strokeRect(126, 29, 10, 15); // right
+    // ctx.strokeRect(124, 22, 14, 5); // right cap
+    ctx.beginPath();
+    ctx.moveTo(124, 22);
+    ctx.lineTo(127, 22);
+    ctx.lineTo(127, 25);
+    ctx.lineTo(131, 25);
+    ctx.lineTo(131, 22);
+    ctx.lineTo(135, 22);
+    ctx.lineTo(135, 25);
+    ctx.lineTo(137, 25);
+    ctx.lineTo(138, 22);
+    ctx.lineTo(138, 30);
+    ctx.lineTo(124, 30);
+    ctx.closePath();
+    ctx.stroke();
+
+    // outer towers
+    ctx.strokeRect(10, 105, 25, 65); //lower left
+    ctx.strokeRect(7, 89, 32, 14); // lower left cap
+
+    ctx.strokeRect(40, 73, 20, 40); // upper left
+    ctx.strokeRect(37, 57, 26, 14); // upper left cap
+
+    ctx.strokeRect(168, 105, 25, 65); //lower right
+    ctx.strokeRect(165, 89, 32, 14); // lower right cap
+
+    ctx.strokeRect(145, 73, 20, 40); //upper right
+    ctx.strokeRect(142, 57, 26, 14); // upper right cap
   }
 }
 window.addEventListener("load", draw);
+
+// todo
+// function drawPillar() {
+
+// }
